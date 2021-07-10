0f47801dba751009a61146db21d5a92629254fe8
woasidh <woasidh1@naver.com>
2021-07-10 13:54:13 +0900

    get api2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:randomStr', function(req, res, next){
+  res.send('jijo');
+});
+
 module.exports = router;
